285425ef7ed57d20041ed3210611469ccc78c095
Farrukh Askari <farrukh.askari01@gmail.com>
2016-02-14 22:50:47 +0500

    Initial Commit

---
+++ GettingStartedWithJSON2Tests/GettingStartedWithJSON2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  GettingStartedWithJSON2Tests.swift
+//  GettingStartedWithJSON2Tests
+//
+//  Created by Syed Askari on 14/02/2016.
+//  Copyright © 2016 Syed Askari. All rights reserved.
+//
+
+import XCTest
+@testable import GettingStartedWithJSON2
+
+class GettingStartedWithJSON2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
